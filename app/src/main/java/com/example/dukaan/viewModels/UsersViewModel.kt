3e2a85cd1c9b797360b1d5ae60b0dd2bcdb7d71f
unknown <siddharth9k9@gmail.com>
2021-03-10 09:54:22 +0530

    homepage

---
+++ app/src/main/java/com/example/dukaan/viewModels/UsersViewModel.kt
@@ -0,0 +1,30 @@
+package com.example.dukaan.viewModels
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import com.example.dukaan.localDatabase.DukaanRoomDAO
+import com.example.dukaan.localDatabase.StoreEntity
+import com.example.dukaan.localDatabase.UsersEntity
+import com.example.dukaan.repository.UsersRepository
+
+class UsersViewModel(val dukaanRoomDAO: DukaanRoomDAO): ViewModel() {
+    val userRepository = UsersRepository(dukaanRoomDAO)
+    suspend fun isUserExists(phonenumber:String): Boolean {
+        return userRepository.isUserExists(phonenumber)
+    }
+
+    suspend fun addNewuser(usersEntity: UsersEntity) {
+        userRepository.adNewUser(usersEntity)
+    }
+
+    suspend fun fetchUser(phonenumber: String): LiveData<List<UsersEntity>> {
+        return userRepository.fetchUser(phonenumber)
+    }
+
+    suspend fun insertStore(storeEntity: StoreEntity){
+        return userRepository.insertStore(storeEntity)
+    }
+    suspend fun updateUser(usersEntity: UsersEntity){
+        userRepository.updateUser(usersEntity)
+    }
+}
